3a96f6d972a0edf58e93a88f38ce1d2f9a449592
Veronica Person <veronicalperson@gmail.com>
2022-05-22 22:45:14 -0400

    update js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-function generatePassword() {
+var password = document.querySelector("#password")
+
+
   var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -14,7 +16,7 @@ function generatePassword() {
 
   uppercaseArray [1]
 
-  var numCharacter = prompt ("How many number of Characters do you want between 8 and 128?");
+  var numCharacter = confirm ("How many number of Characters do you want between 8 and 128?");
   var numbers = confirm ("Do you want numbers in your password?");
   var uppercases = confirm ("Do you want Uppercases in your password?");
   var lowercases = confirm ("Do you want lowercases in your password?");
@@ -42,15 +44,17 @@ console.log(resultArray)
 
 for (var i = 0; i < numCharacter; i++){
       
-  userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
-  }
+    randomString += optionsVariable.charAt(Math.floor(Math.random() * optionsVariable.length));
+}
+
+password.value = randomString;
 
  
 
 // Write password to the #password input
 function writePassword(characterAmount, IncludeUppercase, IncludeNumbers, IncludeSymbols)
  {
-  var password = generatePassword();
+  var pwTextArea = document.getElementById("password");
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
